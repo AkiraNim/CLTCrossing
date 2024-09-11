1a0c770ca01127ece9601eef7a5ea2758518ea1d
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-09-11 20:13:32 -0300

    update

--- teste3d/Scripts/coins_container.gd
+++ teste3d/Scripts/coins_container.gd
@@ -1,7 +1,7 @@
 extends HBoxContainer
 
 
-@onready var coin_label: Label = $coinLabel
+@onready var life_label: Label = $"../../HUD2/lifeContainer/lifeLabel"
 
-func update_coin(amount: int):
-	coin_label.text = "%d" % amount
+func update_life(amount: int):
+	life_label.text = "%d" % amount

5711f9d067529e99921fcf39c1c524ade1f73d47
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-09-13 19:58:14 -0300

    update

--- teste3d/Scripts/game_over.gd
+++ teste3d/Scripts/game_over.gd
@@ -3,7 +3,6 @@ extends CanvasLayer
 @onready var retry_button: Button = $ColorRect/menuGameOver/retryButton
 
 
-
 func _on_retry_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()

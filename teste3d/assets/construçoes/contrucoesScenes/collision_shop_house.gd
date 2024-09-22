028d8180067fdb33bb2aee361e67e6186b54a0ba
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-09-22 20:00:33 -0300

    update assets + scenes

--- teste3d/assets/construçoes/contrucoesScenes/collision_shop_house.gd
+++ teste3d/assets/construçoes/contrucoesScenes/collision_shop_house.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/BuildsScenes/shop_house_1.tscn")

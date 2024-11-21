a9f7b926d9dea4712eecb55bade464e083b9211d
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-11-21 20:31:16 -0300

    update save&load

--- teste3d/Savegame/save_load.gd
+++ teste3d/Savegame/save_load.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 func save_game():
 	var saved_game:SavedGame = SavedGame.new()
 	
@@ -7,16 +8,17 @@ func save_game():
 	saved_game.inventory = PlayerManager.player.inventory_data
 	saved_game.equip_inventory = PlayerManager.player.equip_inventory_data
 	saved_game.money = PlayerManager.player.money
+	saved_game.missions_complete = PlayerManager.player.missions_complete
 	ResourceSaver.save(saved_game, "user://savegame.tres")
-saved_game.missions_complete = PlayerManager.player.missions_complete
-
+	
 func load_game():
 	var saved_game: SavedGame = load("user://savegame.tres") as SavedGame
 	PlayerManager.player.global_position = saved_game.player_position
 	PlayerManager.player.money = saved_game.money
+	for i in range(PlayerManager.player.inventory_data.slot_datas.size()):
+		PlayerManager.player.inventory_data.slot_datas[i] = saved_game.inventory.slot_datas[i]
+	for i in range(PlayerManager.player.equip_inventory_data.slot_datas.size()):
+		PlayerManager.player.equip_inventory_data.slot_datas[i] = saved_game.equip_inventory.slot_datas[i]
 	PlayerManager.player.inventory_data.inventory_updated.emit(PlayerManager.player.inventory_data)
 	PlayerManager.player.equip_inventory_data.inventory_updated.emit(PlayerManager.player.equip_inventory_data)
-
-PlayerManager.player.missions_complete = saved_game.missions_complete
-	print(PlayerManager.player.inventory_data.slot_datas[0].quantity)
-
+	PlayerManager.player.missions_complete = saved_game.missions_complete

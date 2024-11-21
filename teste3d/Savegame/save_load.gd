7e015097c8eb92af6f2bd73b3d1373ee078d67d8
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-11-21 08:06:13 -0300

    Atualizar o save_load.gd

--- teste3d/Savegame/save_load.gd
+++ teste3d/Savegame/save_load.gd
@@ -8,6 +8,7 @@ func save_game():
 	saved_game.equip_inventory = PlayerManager.player.equip_inventory_data
 	saved_game.money = PlayerManager.player.money
 	ResourceSaver.save(saved_game, "user://savegame.tres")
+saved_game.missions_complete = PlayerManager.player.missions_complete
 
 func load_game():
 	var saved_game: SavedGame = load("user://savegame.tres") as SavedGame
@@ -17,4 +18,6 @@ func load_game():
 	PlayerManager.player.money = saved_game.money
 	PlayerManager.player.inventory_data.inventory_updated.emit(PlayerManager.player.inventory_data)
 	PlayerManager.player.equip_inventory_data.inventory_updated.emit(PlayerManager.player.equip_inventory_data)
-	print(PlayerManager.player.inventory_data.slot_datas[0].quantity)
+
+PlayerManager.player.missions_complete = saved_game.missions_complete	print(PlayerManager.player.inventory_data.slot_datas[0].quantity)
+

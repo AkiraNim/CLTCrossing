c0a8f6e940995f8c589d1eb9960d847a63e0cb91
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-12-09 18:43:53 -0300

    updates

--- CltCrossing/Scripts/sair.gd
+++ CltCrossing/Scripts/sair.gd
@@ -2,6 +2,6 @@ extends StaticBody3D
 
 
 func player_interact():
-	Globals.next_scene = "res://scenes/mapa.tscn"
+	Globals.next_scene = "res://scenes/mapa_principal.tscn"
 	get_tree().change_scene_to_packed(Globals.loading_screen)
 	SaveLoad.save_inventory()

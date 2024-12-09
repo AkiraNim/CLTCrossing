1858510e0524f281451a22452ba5f6416a7c7ca1
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-12-09 18:01:05 -0300

    Update main_menu.gd

--- CltCrossing/scenes/main_menu.gd
+++ CltCrossing/scenes/main_menu.gd
@@ -8,10 +8,11 @@ func _process(delta: float) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 func _on_btn_jogar_pressed() -> void:
-	Globals.next_scene = "res://scenes/mapa.tscn"
-	get_tree().change_scene_to_packed(Globals.loading_screen)
 	if Dialogic.current_timeline == null:
 		Dialogic.start('intro')  # Diálogo específico
+	Globals.next_scene = "res://scenes/mapa_principal.tscn"
+	get_tree().change_scene_to_packed(Globals.loading_screen)
+
 
 func _on_btn_sair_pressed() -> void:
 	get_tree().quit()

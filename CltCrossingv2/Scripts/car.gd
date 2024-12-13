f16e7a3e4366a368ce233dcdaa270674b8b170c1
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-12-13 17:39:19 -0300

    update v2

--- CltCrossingv2/Scripts/car.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-const WALK_SPEED = 3.0
-
-@onready var path_follow_3d: PathFollow3D = $".."
-
-func _process(delta: float) -> void:
-	path_follow_3d.progress += WALK_SPEED * delta
--- CltCrossingv2/Scripts/coins_container.gd
+++
@@ -1,7 +0,0 @@
-extends HBoxContainer
-
-
-@onready var coin_label: Label = $coinLabel
-
-func update_coin(amount: int):
-	coin_label.text = "%d" % amount
